2508cfb6abce0ff3a3c8d968ab847a05dbb27868
unknown <aaryan.mishra.soton@gmail.com>
2024-10-30 15:55:28 +0000

    created credit and debit methods

--- src/main/java/com/lbg/unittesting/Bank.java
+++ src/main/java/com/lbg/unittesting/Bank.java
@@ -3,9 +3,33 @@ package com.lbg.unittesting;
 public class Bank {
 
     private String firstname, lastname;
+
+    private double balance;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    private void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void credit(double amountCredited) {
+        double currentBalance = this.getBalance();
+        currentBalance += amountCredited;
+        this.setBalance(currentBalance);
+    }
+
+    public void debit(double amountDebited) {
+        double currentBalance = this.getBalance();
+        currentBalance -= amountDebited;
+        this.setBalance(currentBalance);
+    }
+
     public Bank(String firstname, String lastname){
         this.firstname = firstname;
         this.lastname = lastname;
+        this.setBalance(0.0);
     }
 
     public String getAccountHolderName(){
--- src/test/java/com/lbg/unittesting/BankTest.java
+++ src/test/java/com/lbg/unittesting/BankTest.java
@@ -10,4 +10,20 @@ public class BankTest {
         assertEquals("Anika Hussain", name);
     }
 
+    @Test
+    public void testCreditFunction() {
+        Bank account = new Bank("Aaryan","Mishra");
+        account.credit(100.0);
+        double resultingBalance = account.getBalance();
+        assertEquals(100.0, resultingBalance);
+    }
+
+    @Test
+    public void testDebitFunction() {
+        Bank account = new Bank("Nassim", "Karimi");
+        account.credit(100.0);
+        account.debit(50.0);
+        double resultingBalance = account.getBalance();
+        assertEquals(50.0, resultingBalance);
+    }
 }
