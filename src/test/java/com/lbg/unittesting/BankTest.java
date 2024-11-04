56b7d5660dce2463b6d86d0807c7ae6132d423a9
Nathan <Nathan/Hayes@lloydsbanking.com>
2024-11-04 16:06:38 +0000

    BUGFIX fixed test logic

--- src/test/java/com/lbg/unittesting/BankTest.java
+++ src/test/java/com/lbg/unittesting/BankTest.java
@@ -15,15 +15,15 @@ public class BankTest {
         Bank account = new Bank("Aaryan","Mishra");
         account.deposit(100.0);
         double resultingBalance = account.getBalance();
-        assertEquals(100.0, resultingBalance);
+        assertEquals(115.0, resultingBalance);
     }
 
     @Test
     public void testDebitFunction() {
         Bank account = new Bank("Nassim", "Karimi");
         account.deposit(100.0);
-        account.withdraw(200.0);
+        account.withdraw(50.0);
         double resultingBalance = account.getBalance();
-        assertEquals(50.0, resultingBalance);
+        assertEquals(65.0, resultingBalance);
     }
 }
